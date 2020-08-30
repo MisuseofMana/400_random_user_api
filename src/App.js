705ad09e479e42a7eae2d732e3be573305d20ce0
Sean <contact@seanyager.com>
2020-08-29 21:31:42 -0500

    intial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import User from './components/User'
+import './App.css';
+
+//comment to allow branch pull request
+
+class App extends Component {
+  constructor (props){
+    super (props);
+    this.state = {
+      data: [],
+    }
+  }
+
+  //functions
+  componentDidMount() {
+    fetch('https://randomuser.me/api?results=25')
+        .then(res => res.json())
+        .then(examples => {
+            this.setState({ data: examples.results })
+        })
+  }
+
+  //render
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {
+             this.state.data.map((user, index) => {
+              return <User 
+                key={index} 
+                userName={user.name.first + ' ' + user.name.last} 
+                userImg={user.picture.thumbnail}
+                
+                userAge={user.dob.age} 
+                userCountry={user.location.country}
+                userState={user.location.state}
+                userCity={user.location.city}
+                userPhone={user.cell}
+              />
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
